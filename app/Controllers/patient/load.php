64a551f80afbc6ce66461b056b21a7508b220281
Sebs <sebastianruizj2014@gmail.com>
2023-11-16 14:25:41 -0500

    Resumen

--- app/Controllers/patient/load.php
+++ app/Controllers/patient/load.php
@@ -123,7 +123,7 @@ if ($result->rowCount() > 0) {
 						<div class="modal-dialog modal-lg modal-dialog-scrollable">
 							<div class="modal-content">
 								<div class="modal-header">
-									<h1 class="modal-title fs-5">Resumen de Paciente</h1>
+									<h1 class="modal-title fs-5">Resumen</h1>
 									<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 								</div>
 								<div class="modal-body">
